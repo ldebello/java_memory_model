c61f3636458d0a2aa52ca8d910e0998a876419e2
Luis de Bello <luis.debello@mulesoft.com>
2016-08-01 21:17:36 -0300

    Adding more comments for JCStress

--- JCStressTutorial/src/jcstress/java/ar/com/javamemorymodel/jcstress/BitSetConcurrencyTest.java
+++ JCStressTutorial/src/jcstress/java/ar/com/javamemorymodel/jcstress/BitSetConcurrencyTest.java
@@ -23,7 +23,7 @@ import org.openjdk.jcstress.infra.results.BooleanResult2;
  */
 @JCStressTest
 @Description("Utilizando BitSet desde multiples threads")
-@State 
+@State
 @Outcome(id = "[true, true]", expect = Expect.ACCEPTABLE, desc = "Los updates se ven de forma correcta")
 @Outcome(id = "[true, false]", expect = Expect.ACCEPTABLE_INTERESTING, desc = "Thread 2 sobrrescribre Thread 1")
 @Outcome(id = "[false, true]", expect = Expect.ACCEPTABLE_INTERESTING, desc = "Thread 1 sobrrescribre Thread 2")
@@ -31,19 +31,19 @@ public class BitSetConcurrencyTest {
 
 	private BitSet sharedBitSet = new BitSet();
 
-    @Actor
-    public void thread1() {
-        sharedBitSet.set(1);
-    }
+	@Actor
+	public void thread1() {
+		sharedBitSet.set(1);
+	}
 
-    @Actor
-    public void thread2() {
-        sharedBitSet.set(2);
-    }
-    
-    @Arbiter
-    public void observe(BooleanResult2 result) {
-    	result.r1 = sharedBitSet.get(1);
-    	result.r2 = sharedBitSet.get(2);
-    }
+	@Actor
+	public void thread2() {
+		sharedBitSet.set(2);
+	}
+
+	@Arbiter
+	public void observe(BooleanResult2 result) {
+		result.r1 = sharedBitSet.get(1);
+		result.r2 = sharedBitSet.get(2);
+	}
 }
